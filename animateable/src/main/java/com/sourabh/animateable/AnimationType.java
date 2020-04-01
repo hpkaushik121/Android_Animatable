01cf7a37340dad1011f980c8811804905f3893e9
sourabh kaushik <sourabh.kaushik@cg-infotech.com>
2020-04-01 15:02:11 +0530

    Initial Commit

---
+++ animateable/src/main/java/com/sourabh/animateable/AnimationType.java
@@ -0,0 +1,39 @@
+package com.sourabh.animateable;
+
+public class AnimationType {
+    public  static final int bounce=1;
+    public static final int bounceInRight=2;
+    public static final int bounceInLeft=3;
+    public static final int bounceInUp=4;
+    public static final int bounceInDown=5;
+    public static final int flash=6;
+    public static final int rubberBand=7;
+    public static final int pulse=8;
+    public static final int shake=9;
+    public static final int swing=10;
+    public static final int wobble=11;
+    public static final int tada=12;
+    public static final int standUp=13;
+    public static final int wave=14;
+    public static final int rollin=15;
+    public static final int rollOut=16;
+    public static final int bounceIn=17;
+    public static final int fadeInUp=18;
+    public static final int fadeInDown=19;
+    public static final int fadeInLeft=20;
+    public static final int fadeInRight=21;
+    public static final int fadeOut=22;
+    public static final int fadeOutUp=23;
+    public static final int fadeOutDown=24;
+    public static final int fadeOutRight=25;
+    public static final int fadeOutLeft=26;
+    public static final int flipInX=27;
+    public static final int flipInY=28;
+    public static final int flipOutX=29;
+    public static final int flipOutY=30;
+    public static final int rotateIn=31;
+    public static final int rotateInDownLeft=32;
+    public static final int rotateInDownRight=33;
+    public static final int rotateInUpLeft=34;
+    public static final int rotateInUpRight=35;
+}
